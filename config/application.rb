aee28b9356aff4aab60359ff1bde1f97b8186ec3
Will Richman <williamrichman@gmail.com>
2012-07-27 21:43:31 -0400

    Add user microposts

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ end
 
 module SampleApp
   class Application < Rails::Application
+  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -54,7 +55,7 @@ module SampleApp
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    # config.active_record.whitelist_attributes = true
+    config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
